187b06f50d774c2b5026e20d6b003b0bc11152e1
André Faria <andfar10@gmail.com>
2024-03-13 20:52:20 +0000

    Softskill letter animation

--- src/components/canvas/Blackhole.jsx
+++ src/components/canvas/Blackhole.jsx
@@ -4,36 +4,49 @@
 import React, { Suspense, useEffect, useState, useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF, Sphere } from "@react-three/drei";
-import Ship from "./Ship"
+import Ship from "./Ship";
 import CanvasLoader from "../Loader";
 import Softskill from "./Softskill";
-import { color } from "framer-motion";
+import { color, motion } from "framer-motion";
 import * as THREE from "three";
+import {
+  Debug,
+  Physics,
+  useBox,
+  usePlane,
+  useSphere,
+  useTrimesh,
+  useCylinder,
+  useConvexPolyhedron,
+} from "@react-three/cannon";
 
-const Blackhole = ({ isMobile, active, setActive }) => {
+const Blackhole = ({ isMobile, active, setActive, setVisible }) => {
   const blackhole = useGLTF("./planets/blackhole/scene.gltf");
-  const ref = useRef()
-  const [mult, setMult] = useState(5)
-  const minScale = 1
-  const maxScale = 14
-  
+  const ref = useRef();
+  const [mult, setMult] = useState(5);
+  const minScale = 1;
+  const maxScale = 14;
+
   useFrame((state, delta) => {
-    ref.current.rotation.y += delta
-    console.log(ref.current.scale.x);
-    if(active){
-      if(mult == 5 && ref.current.scale.x > maxScale){
-        setActive(false)
-        setMult(-5)
+    ref.current.rotation.y += delta;
+    if (active) {
+      if (mult == 5 && ref.current.scale.x >= maxScale) {
+        setActive(false);
+        setMult(-5);
+        setVisible(true);
       }
       if (mult == -5 && ref.current.scale.x <= minScale) {
-        setActive(false)
-        setMult(5)
+        setActive(false);
+        setMult(5);
       }
-      ref.current.scale.x += mult*delta
-      ref.current.scale.y += mult*delta
-      ref.current.scale.z += mult*delta
+      if(ref.current.scale.x < maxScale){
+        setVisible(false);
+      }
+      ref.current.scale.x += mult * delta;
+      ref.current.scale.y += mult * delta;
+      ref.current.scale.z += mult * delta;
     }
-  })
+  });
 
   return (
     <mesh ref={ref}>
@@ -43,22 +56,16 @@ const Blackhole = ({ isMobile, active, setActive }) => {
         position={isMobile ? [0, 0, 0] : [0, -0.5, 0]}
         rotation={[0.0, 0.0, 0.0]}
       />
-      <Sphere 
-        position={[0, -.5, 0]}
-        scale={1}
-      >
-        <meshBasicMaterial
-          side={THREE.DoubleSide}
-          color="#000"
-        />
+      <Sphere position={[0, -0.5, 0]} scale={1}>
+        <meshBasicMaterial side={THREE.DoubleSide} color="#000" />
       </Sphere>
     </mesh>
   );
 };
 
-const BlackholeCanvas = ({ref, active, setActive}) => {
+const BlackholeCanvas = ({ ref, active, setActive }) => {
   const [isMobile, setIsMobile] = useState(false);
-  
+  const [visible, setVisible] = useState(false);
   useEffect(() => {
     // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
@@ -82,29 +89,45 @@ const BlackholeCanvas = ({ref, active, setActive}) => {
 
   return (
     <Canvas
-      frameloop='always'
+      frameloop="always"
       shadows
       dpr={[1, 2]}
-      camera={{ position: [0, 10, 0], fov: 25}}
-      gl={{ preserveDrawingBuffer: true, alpha: true}}
+      camera={{ position: [0, 10, 0], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true, alpha: true }}
     >
-      <axesHelper args={[10]}/>
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
           enableZoom={false}
           enablePan={false}
+          enableRotate={false}
         />
-        <Blackhole isMobile={isMobile} active={active} setActive={setActive}/>
+        <Blackhole
+          isMobile={isMobile}
+          active={active}
+          setActive={setActive}
+          setVisible={setVisible}
+          onClick={() => setVisible(!visible)}
+        />
         {/* <Ship/> */}
-        <Softskill/>
+
+        <Softskill
+          lsKey={"softskill"}
+          visible={visible}
+          x={Math.random() * 2 - 2}
+          y={Math.random() * 1 - 1}
+        />
+        <Softskill
+          lsKey={"softskill"}
+          visible={visible}
+          x={Math.random() * 2 - 2}
+          y={Math.random() * 1 - 1}
+        />
       </Suspense>
       <Preload all />
     </Canvas>
   );
 };
-
-
 
 export default BlackholeCanvas;
--- src/components/canvas/Softskill.jsx
+++ src/components/canvas/Softskill.jsx
@@ -1,18 +1,65 @@
-import React from 'react'
-import {Text} from '@react-three/drei'
-const Softskill = () => {
+import React, { useEffect, useState, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { Text, Float } from "@react-three/drei";
+import * as THREE from "three";
+
+const Softskill = ({ lsKey, visible, x, y }) => {
+  const ref = useRef();
+  const mult = 1;
+  const [posX, setX] = useState(x);
+  const [posY, setY] = useState(y);
+  const maxScale = 0.4;
+  const minScale = 0;
+  const timeout = Math.random() * 3000 + 1000;
+  const [readyToAppear, setReadyToAppear] = useState(false);
+  const [textState, setTextState] = useState(0);
+
+  useEffect(() => {
+    if (visible) {
+      const timer = setTimeout(() => {
+        setReadyToAppear(true);
+        ref.current.visible = true;
+      }, timeout);
+      return () => clearTimeout(timer);
+    }
+  }, [visible, timeout]);
+
+  useFrame((state, delta) => {
+    console.log(textState, visible, readyToAppear);
+    if (visible && readyToAppear && textState == 0) {
+      if (ref.current.fontSize > maxScale) {
+        setTextState(1);
+      } else if (textState == 0) {
+        ref.current.fontSize += mult * delta;
+      }
+    } else if (!visible && textState == 1) {
+      if (ref.current.fontSize < minScale) {
+        setTextState(0);
+        setReadyToAppear(false);
+        ref.current.visible = false;
+      } else {
+        ref.current.fontSize -= mult * delta;
+      }
+    }
+  }, []);
+
   return (
     <>
+      <Float>
         <Text
-            color="white"
-            fontSize={0.2}
-            position={[0, 0, 0]}
-            rotation={[0, 0, 0]}
+          ref={ref}
+          color="white"
+          fontSize={0}
+          position={[posX, posY, 0]}
+          rotation={[0, 0, 0]}
+          scale={1}
+          visible={false}
         >
-           Soft Skills
+          S
         </Text>
+      </Float>
     </>
-  )
-}
+  );
+};
 
-export default Softskill
+export default Softskill;
