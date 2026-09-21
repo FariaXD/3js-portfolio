aecb01d130a7e3df1531283247a3d57f8cf3189d
André Faria <58751939+FariaXD@users.noreply.github.com>
2026-09-21 18:09:34 +0100

    Fix email address in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -38,7 +38,7 @@ const Contact = () => {
         from_name: form.name,
         to_name: "Andre",
         from_email: form.email,
-        to_email: "fariafacto2+contact@gmail.com",
+        to_email: "fariafactor2+contact@gmail.com",
         message: form.message
       },
       'xfq9swVWwAPBNvyTL'
